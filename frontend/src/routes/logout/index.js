5738148d3e8179417d8cfa354141a727864cbeb2
Ciprian Ionescu <me@ciprian-ionescu.com>
2020-05-07 19:35:18 +0300

    reset more caches

--- frontend/src/routes/logout/index.js
+++ frontend/src/routes/logout/index.js
@@ -1,10 +1,12 @@
 import { h, Component } from 'preact';
 import { route } from 'preact-router';
+import { client } from '../../components/app';
 
 class Logout extends Component {
 	componentWillMount() {
 		console.log('wow');
 		sessionStorage.clear('token');
+		client.cache.reset();
 		route('/', true);
 	}
 
