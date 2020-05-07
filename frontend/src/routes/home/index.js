d27ae65d0634305beeeff88a49b325a41e0e9e3d
Ciprian Ionescu <me@ciprian-ionescu.com>
2020-05-07 15:32:43 +0300

    replaced placeholder text

--- frontend/src/routes/home/index.js
+++ frontend/src/routes/home/index.js
@@ -18,7 +18,7 @@ const Home = () => (
 	<article>
 		<div class="relative">
 			<header class="bg-indigo-darkest bg-grid text-white px-8 pt-16 pb-24 mb-8 slanted" >
-				<h1 class="text-5xl max-w-sm">Olimpiada municipală de informatică 2019</h1>
+				<div class="container mx-auto"><h1 class="text-5xl max-w-sm">Easy Appeal System - InfoEducație 2020</h1></div>
 			</header>
 			<div class="hero-button absolute text-center">
 				<Link class="px-6 py-3 bg-indigo-dark hover:bg-indigo active:bg-indigo-darker shadow-md tems-center text-indigo-lightest font-bold no-underline leading-none rounded-full inline-flex" href="/tasks">
@@ -27,12 +27,17 @@ const Home = () => (
 				</Link>
 			</div>
 		</div>
-		<p class="p-4 md:px-8">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec urna tortor, dignissim a ante non, aliquam gravida ante. Duis eleifend nulla eu odio fermentum, in laoreet felis dapibus. Nullam hendrerit quam elit, pellentesque varius quam bibendum in. Cras felis arcu, cursus eget placerat sed, cursus non dui. Nulla lobortis sapien varius, consectetur felis placerat, faucibus quam. Nam sed quam tincidunt, cursus enim vel, auctor sapien. Curabitur pulvinar nibh massa.
-		</p>
-		<p class="p-4 md:px-8">
-			Mauris euismod tempus nibh, a elementum elit hendrerit id. Nunc faucibus, dolor nec luctus volutpat, quam nulla vehicula nulla, in tempus tellus elit a felis. Nullam in purus vel sapien ultricies venenatis sit amet non nunc. Nullam non suscipit quam, mattis pulvinar ante. Suspendisse eu tortor sed mauris malesuada posuere a vel ipsum. Donec ultricies a orci et sollicitudin. Morbi luctus nec orci eget viverra. Pellentesque magna neque, commodo sit amet suscipit a, vulputate ac dui.
-		</p>
+		<div class="container mx-auto">
+			<p class="p-4 md:px-8">
+				Olimpiada de Informatică, faza pe județ, mereu a fost o fază care la București stârnește o oarecare dezordine an de an. Datorită unui regulament vechi și outdated în raport cu tehnologia modernă, procese precum văzutul baremelor și a rezultatului evaluării necesită depunere de contestație și birocrație multă, iar mulți aleg să sară peste asta și să nu știe niciodată de ce au luat punctajul pe care l-au luat. De asemenea, odată cu noua legislație privind majoritatea Olimpiadelor școlare, sectoarele din București au fost clasificate ca și județe iar faza pe sector a fost eliminată, ceea ce a dus la un influx mare de participanți. Toate centrele de examen au fost puse pe post de centre de contestații ca să facă față, obligând astfel deschiderea școlilor de dimineață pentru examen, și închiderea lor de-abia seara după contestații.
+			</p>
+			<p class="p-4 md:px-8">
+				EAS - Easy Appeal System - caută să rezolve aceste probleme. Utilizând coduri unice fiecărui concurent, EAS oferă o soluție modernă și securizată și pune la dispoziția fiecărui elev o platformă ușor de folosit unde poate să își vadă rezultatul și raportul evaluării automate, și unde poate opțional să inițieze procesul de depunere a contestațiilor.
+			</p>
+			<p class="p-4 md:px-8">
+				Fiecare concurent primește la început de probă o fișă pe care este notat ID-ul său, un cod de acces, și linkul de la instanța publică de EAS. De acolo, un concurent poate vedea statusul evaluării, rezultatul evaluării, sursa proprie, și va putea depune conestație pentru una sau mai multe probleme direct din interfață.
+			</p>
+		</div>
 	</article>
 );
 
